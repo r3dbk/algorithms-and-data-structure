f964747a1a2cb573cc8386347dea60a154900892
Sviatoslav <113039372+r3dbk@users.noreply.github.com>
2025-10-01 10:25:48 +0300

    red

---
+++ KR2/2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "enter a number to factorize: ";
+    cin >> number;
+    // решето Эратосфена
+    int x = number;
+    vector<bool> prostoe(x + 1, true);
+    prostoe[0] = prostoe[1] = false;
+    // отмечаем составные числа
+    for (int i = 2; i * i <= x; i++) {
+        if (prostoe[i]) {
+            for (int j = i * i; j <= x; j += i) {
+                prostoe[j] = false;
+            }
+        }
+    }
+    // Факторизация с использованием решета
+    vector<int> factors;
+    int temp = number;
+
+    // Перебираем только простые делители из решета
+    for (int i = 2; i <= x; i++) {
+        if (prostoe[i] && temp % i == 0) {
+            while (temp % i == 0) {
+                factors.push_back(i);
+                temp /= i;
+            }
+        }
+        if (temp == 1) break;
+    }
+    // Выводим результат факторизации
+    cout << "prime factors of " << number << ": ";
+    for (int factor : factors) {
+        cout << factor << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
